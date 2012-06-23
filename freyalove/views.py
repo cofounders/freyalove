fe3e89843ad8766c88bbb90705bed010d4e1497a
Kenny Shen <kenny@northpole.sg>
2012-06-23 14:34:58 +0800

    i messed up the indent. oh whitespaces.

--- freyalove/views.py
+++ freyalove/views.py
@@ -3,12 +3,12 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 def homepage(request):
-	"""
-	Show me the homepage!
-	"""
-	template = "home.html"
+    """
+    Show me the homepage!
+    """
+    template = "home.html"
 
-	resp = render_to_response(template, {
+    resp = render_to_response(template, {
     }, context_instance=RequestContext(request))
 
     return resp
