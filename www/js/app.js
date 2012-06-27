0afc4214f987e50c4ad7a2cc704524a2fde6c042
Sebastiaan Deckers <cbas@pandion.be>
2012-06-27 21:58:46 +0800

    Frontend stack: h5bp, requirejs, grunt, stylus

---
+++ www/js/app.js
@@ -0,0 +1,4 @@
+require(['google-analytics'], function(ga) {
+	ga.track('UA-32964267-1');
+	console.log('Success!');
+});
---
+++ www/js/google-analytics.js
@@ -0,0 +1,19 @@
+define(function() {
+	return {
+		track: function(accountId) {
+			var _gaq = window._gaq = _gaq || [],
+					ga = document.createElement('script'),
+					s = document.getElementsByTagName('script')[0];
+
+			_gaq.push(['_setAccount', accountId]);
+			_gaq.push(['_trackPageview']);
+
+			ga.type = 'text/javascript';
+			ga.async = true;
+			ga.src = ('https:' == document.location.protocol ?
+				'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+
+			s.parentNode.insertBefore(ga, s);
+		}
+	};
+});
