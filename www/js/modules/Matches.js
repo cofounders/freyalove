f0a9a35b689461fd78addaf052d19fcba3042349
Sebastiaan Deckers <cbas@pandion.be>
2012-07-24 19:33:45 +0800

    Fixed URLs for matches

--- www/js/modules/Matches.js
+++ www/js/modules/Matches.js
@@ -1,65 +1,66 @@
 define(['jQuery', 'Underscore', 'Mustache', 'Backbone', 'app'],
 function($, _, Mustache, Backbone, app) {
 
-  var Views = {},
-    Collections = {};
+	var Views = {},
+		Collections = {};
 
-  var Model = Backbone.Model.extend({
-  });
+	var Model = Backbone.Model.extend({
+	});
 
-  Collections.Singles = Backbone.Collection.extend({
-    model: Model,
-    url: function () {
-      return app.api + '/users/:id/matches/recommendations/'; //get the correct url
-    },
-    parse: function (response) {
-      return response.matches;
-    }
-  });
-  Collections.Couples = Backbone.Collection.extend({
-    model: Model,
-    url: function () {
-      return app.api + '/users/:id/matches/recommendations/'; //get the correct url
-    },
-    parse: function (response) {
-      return response.matches;
-    }
-  });
+	Collections.Singles = Backbone.Collection.extend({
+		model: Model,
+		url: function () {
+			return app.api + '/users/' + app.session.id + '/matches/recommendations';
+		},
+		parse: function (response) {
+			return response.matches;
+		}
+	});
 
-  Views.Singles = Backbone.View.extend({
-    template: 'matches/singles',
-    initialize: function () {
-      this.collection.on('reset', function () {
-        this.render();
-      }, this);
-    },
-    cleanup: function () {
-      this.collection.off(null, null, this);
-    },
-    serialize: function () {
-      return {matches: this.collection.toJSON()};
-    }
-  });
+	Collections.Couples = Backbone.Collection.extend({
+		model: Model,
+		url: function () {
+			return app.api + '/matchmaker/recommendations';
+		},
+		parse: function (response) {
+			return response.matches;
+		}
+	});
+
+	Views.Singles = Backbone.View.extend({
+		template: 'matches/singles',
+		initialize: function () {
+			this.collection.on('reset', function () {
+				this.render();
+			}, this);
+		},
+		cleanup: function () {
+			this.collection.off(null, null, this);
+		},
+		serialize: function () {
+			return {matches: this.collection.toJSON()};
+		}
+	});
 
 
-  Views.Couples = Backbone.View.extend({
-    template: 'matches/couples',
-    initialize: function () {
-      this.collection.on('reset', function () {
-        this.render();
-      }, this);
-    },
-    cleanup: function () {
-      this.collection.off(null, null, this);
-    },
-    serialize: function () {
-      return {matches: this.collection.toJSON()};
-    }
-  });
+	Views.Couples = Backbone.View.extend({
+		template: 'matches/couples',
+		initialize: function () {
+			this.collection.on('reset', function () {
+				this.render();
+			}, this);
+		},
+		cleanup: function () {
+			this.collection.off(null, null, this);
+		},
+		serialize: function () {
+			return {matches: this.collection.toJSON()};
+		}
+	});
 
-  return {
-    Model: Model,
-    Collections: Collections,
-    Views: Views
-  };
+	return {
+		Model: Model,
+		Collections: Collections,
+		Views: Views
+	};
 });
