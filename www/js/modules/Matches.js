b8bd07603ab3bc56c554807475c13c2d5dd1aef4
Sebastiaan Deckers <cbas@pandion.be>
2012-07-25 21:36:51 +0800

    Fixed API URLs

--- www/js/modules/Matches.js
+++ www/js/modules/Matches.js
@@ -10,7 +10,7 @@ function($, _, Mustache, Backbone, app) {
 	Collections.Singles = Backbone.Collection.extend({
 		model: Model,
 		url: function () {
-			return app.api + '/users/' + app.session.id + '/matches/recommendations';
+			return app.api + 'users/' + app.session.id + '/matches/recommendations';
 		},
 		parse: function (response) {
 			return response.matches;
@@ -20,7 +20,7 @@ function($, _, Mustache, Backbone, app) {
 	Collections.Couples = Backbone.Collection.extend({
 		model: Model,
 		url: function () {
-			return app.api + '/matchmaker/recommendations';
+			return app.api + 'matchmaker/recommendations';
 		},
 		parse: function (response) {
 			return response.matches;
