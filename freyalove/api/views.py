d301cb5e25b539ae31bea812f5417fc4d8a4d957
Kenny Shen <kenny@northpole.sg>
2012-08-10 17:13:37 +0800

    Fix #76

--- freyalove/api/views.py
+++ freyalove/api/views.py
@@ -664,13 +664,13 @@ def create_wink(request, from_profile_id, to_profile_id):
         resp = HttpResponse("Bad request", status=400)
         return resp
 
-def send_message(request, to_profile_id):
+def send_message(request):
     if request.method == "POST":
-        resp_data = {}
-        message = request.POST.get('message', None)
+        recipients = request.POST.getlist('to', [])
+        message = request.POST.get('body', None)
 
-        if not message:
-            resp = HttpResponse("Bad request - message submission missing.", status=400)
+        if not message or not recipients:
+            resp = HttpResponse("Bad request - submission criteria missing.", status=400)
             return resp
         # parse for token in cookie
         cookie = facebook.get_user_from_cookie(request.COOKIES, settings.FACEBOOK_ID, settings.FACEBOOK_SECRET)
@@ -680,22 +680,39 @@ def send_message(request, to_profile_id):
             return resp
         profile = is_registered_user(fetch_profile(cookie["access_token"]))
 
-        try:
-            to_profile = Profile.objects.get(id=int(to_profile_id))
-        except Profile.DoesNotExist:
-            resp = HttpResponse("Bad request - user requested to send message to doesn't exist.", status=400)
+        recipients = [int(x) for x in recipients]
+
+        to_profiles = Profile.objects.filter(id__in=recipients)
+        if to_profiles.count() < 1:
+            resp = HttpResponse("Bad request - user(s) requested to send message to doesn't exist.", status=400)
             return resp
 
-        conversation = has_conversation(profile, to_profile)
+        resp_data = []
 
-        msg = Msg()
-        msg.message = message
-        msg.sender = profile
-        msg.receiver = to_profile
-        msg.conversation = conversation
-        msg.save()
+        for to_profile in to_profiles:
+            conversation = has_conversation(profile, to_profile)
 
-        resp_data["status"] = "Successfully saved message from %d to %d: %s" % (msg.sender.id, msg.receiver.id, msg.message)
+            msg = Msg()
+            msg.message = message
+            msg.sender = profile
+            msg.receiver = to_profile
+            msg.conversation = conversation
+            msg.save()
+
+            resp_dict = {}
+            resp_dict["id"] = msg.id
+            from_summary = {}
+            from_summary["id"] = profile.id
+            from_summary["firstName"] = profile.first_name
+            from_summary["lastName"] = profile.last_name
+            from_summary["photo"] = "http://graph.facebook.com/%s/picture" % profile.fb_username
+            from_summary["points"] = "N/A"
+            resp_dict["from"] = from_summary
+            resp_dict["to"] = to_profile.id
+            resp_dict["body"] = msg.message
+            # TODO: timestamp, status
+            resp_data.append(resp_dict)
+
         resp_json = json.JSONEncoder().encode(resp_data)
         resp = inject_cors(HttpResponse(resp_json, content_type="application/json", status=200))
         return resp
