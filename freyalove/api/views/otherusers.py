efe5ced0775c05ff1bf621ecad8f849c92781267
Kenny Shen <kenny@northpole.sg>
2012-08-24 02:14:06 +0800

    Fix bug in /user/:id/friends/

--- freyalove/api/views/otherusers.py
+++ freyalove/api/views/otherusers.py
@@ -68,7 +68,7 @@ def friends(request, fb_username):
 
     resp_data = []
     if len(friends) > 0:
-        resp_data_ = obj_user_summary([friends])
+        resp_data_ = obj_user_summary(friends)
 
     return inject_cors(HttpResponse(json.JSONEncoder().encode(resp_data), content_type="application/json", status=200))
 
