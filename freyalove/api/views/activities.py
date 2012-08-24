3e105a5f161c34aef18aaf885292c63e86d7e3f5
Kenny Shen <kenny@northpole.sg>
2012-08-24 17:32:38 +0800

    missing import

--- freyalove/api/views/activities.py
+++ freyalove/api/views/activities.py
@@ -2,6 +2,7 @@
 from django.http import HttpResponse
 from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
 from django.db.models import Q
 
 try:
