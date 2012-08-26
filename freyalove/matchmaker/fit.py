091dedaec00f2f01e944942689ed4b615f83d5e7
Kenny Shen <kenny@northpole.sg>
2012-08-26 16:00:42 +0800

    Fix #98

--- freyalove/matchmaker/fit.py
+++ freyalove/matchmaker/fit.py
@@ -1,17 +1,38 @@
 from freyalove.users.models import Profile, Wink
+from freyalove.questionnaire.models import Question, Answer
 # ref: https://github.com/cofounders/freyalove/wiki/Algorithm-Match-Fit
 
 def match_fit(profile_1, profile_2):
-	"""
-	Calculates a score (0 - 100) based on questions answered by 2 profiles.
-	"""
+    """
+    Calculates a score (0 - 100) based on questions answered by 2 profiles.
+    """
 
-	score = 0
+    score = 0.0
+    # Scoring formula: OverallMatchQuality := (sum(QuestionPoints)/Number of Questions))/2 + {0.2 || 0}
 
-	# A mutual wink adds 20%
-	if Wink.objects.has_wink(profile_1, profile_2):
-		if score <= 80:
-			score += 20
-		else:
-			score = 100
-	return score
+
+    # Fetch mutually answered questions
+    questions = Answer.objects.mutual_questions(profile_1, profile_2)
+    n = len(questions)
+    question_points = 0.0
+
+    for q in questions:
+        p1_answer = Answer.objects.get(question=q, profile=profile_1)
+        p2_answer = Answer.objects.get(question=q, profile=profile_2)
+
+        if p1_answer.answer in p2_answer.matches and p2_answer.answer in p1_answer.matches:
+            question_points += 0.2
+        elif p1_answer.answer in p2_answer.matches or p2_answer.answer in p1_answer.matches:
+            question_points += 0.1
+        else:
+            pass
+
+    score += ((questions_points/n)/2)
+
+    # A mutual wink adds 20%
+    if Wink.objects.has_wink(profile_1, profile_2):
+        if score <= :
+            score += .2
+        else:
+            score = 1.0
+    return score * 100.0
--- freyalove/questionnaire/managers.py
+++ freyalove/questionnaire/managers.py
@@ -7,5 +7,12 @@ class QuestionnaireManager(models.Manager):
 	pass
 
 class AnswerManager(models.Manager):
-	pass
+	def mutual_questions(self, profile_1, profile_2):
+		# Returns mutually answered questions between 2 profiles
+		answers_1 = super(AnswerManager, self).get_query_set().filter(profile=profile_1)
+		answers_2 = super(AnswerManager, self).get_query_set().filter(profile=profile_2)
+
+		mutual_questions = set([answer.question for answer in answers_1] + [answer.question.id for answer in answers_2])
+
+		return mutual_questions
 	
